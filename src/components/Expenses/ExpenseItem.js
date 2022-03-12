543a65d0c329b3b747612d59bb2bfb5df585ed89
Alexandre Franca <Alexandre.Franca@bjss.com>
2022-03-12 10:39:46 +0000

    feat: Using useState to update expense title dynamically

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,27 @@
+import React, { useState } from 'react';
+
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
 const ExpenseItem = (props) => {
+  //this return an Array with two item: value, function
+  //array destructuring
+  const [title, setTitle] = useState(props.title);
+  
+  const clickHandler = () =>{
+    setTitle('Title Updated');
+    console.log(title);
+  }
 
   return (
     <Card className='expense-item'>
       <ExpenseDate date = {props.date}></ExpenseDate>
       <div className='expense-item__description'>        
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>£{props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
